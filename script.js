730d75259b5b405fc1b76e53e5003504405dd465
Chaitanya0210 <rayewarchaitanya@gmail.com>
2026-05-30 18:51:06 +0530

    basic commit , Learned about console.log , variable , naming ,scope , objects

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Hello Chaitanya");
+console.log("Google");
+
+age=21;
+age+10;
+
+console.log(age);
+
+name="Chaitanya Rayewar";
+console.log(name);
+
+const product = {
+    productName : "pen",
+    rating : 4,
+    Review : 7002,
+    currentPrice : 270,
+    MRP : 285,
+};
+
+console.log(product);
+
+const profile ={
+    fullName: "Chaitanya Rayewar",
+    posts: 195,
+    followers: 569,
+    following: 4,
+};
+
+console.log(profile);
